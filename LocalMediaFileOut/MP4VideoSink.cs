7e8a2ad00f41cc2e6c9f295007e98671e974b96b
thoxey <tom@flaneer.com>
2022-03-02 11:26:50 +0100

    Writing MP4 out code to exercise native lib (no file out yet)

---
+++ LocalMediaFileOut/MP4VideoSink.cs
@@ -0,0 +1,33 @@
+﻿using FlaneerMediaLib;
+
+namespace LocalMediaFileOut
+{
+    internal class MP4VideoSink : IVideoSink
+    {
+        IEncoder encoder;
+
+        public MP4VideoSink()
+        {
+            if(ServiceRegistry.TryGetService<IEncoder>(out var encoder))
+                this.encoder = encoder;
+            else
+                throw new Exception("No available encoder");
+        }
+
+        public void Capture(int numberOfFrames)
+        {
+            for (int i = 0; i < numberOfFrames; i++)
+            {
+                try
+                {
+                    var ptrToFrame = encoder.GetFrame();
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+                
+            }
+        }
+    }
+}
--- LocalMediaFileOut/Program.cs
+++ LocalMediaFileOut/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace LocalMediaFileOut
+﻿using FlaneerMediaLib;
+
+namespace LocalMediaFileOut
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The current directory is {Directory.GetCurrentDirectory()}");
+            Console.WriteLine($"NvEncVideoSource.dll exists: {File.Exists("NvEncVideoSource.dll")}");
+
+            using MediaEncoder encoder = new MediaEncoder(VideoEncoders.NvEncH264);
+
+            var frameSettings = new FrameSettings()
+            {
+                Height = 1440,
+                Width = 2560,
+                MaxFPS = 60
+            };
+
+            var codecSettings = new H264CodecSettings()
+            {
+                Format = BufferFormat.ARGB,
+                GoPLength = 5
+            };
+
+            encoder.InitVideo(frameSettings, codecSettings);
+
+            MP4VideoSink videoSink = new MP4VideoSink();
+            videoSink.Capture(600);
         }
     }
 }
