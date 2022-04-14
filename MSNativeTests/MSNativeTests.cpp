1604dd29ebedeb74033afe6f43e253943dc2f777
thoxey <tom@flaneer.com>
2022-04-14 10:45:29 +0100

    Swapping to MS testing framework

---
+++ MSNativeTests/MSNativeTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MSNativeTests
+{
+	TEST_CLASS(MSNativeTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
