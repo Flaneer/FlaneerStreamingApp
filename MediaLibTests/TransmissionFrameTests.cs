0bf1c1b742d3984215d20335fa63d531ffea2173
thoxey <tom@flaneer.com>
2022-05-10 09:28:43 +0100

    Adding header size test

---
+++ MediaLibTests/TransmissionFrameTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using FlaneerMediaLib.VideoDataTypes;
+using Xunit;
+
+namespace MediaLibTests;
+
+public class TransmissionFrameTests
+{
+    [Fact]
+    public void DummyTest()
+    {
+        TransmissionVideoFrame frame = new TransmissionVideoFrame();
+        var frameAsBytes = frame.ToUDPPacket();
+        Assert.Equal(frameAsBytes.Length, TransmissionVideoFrame.HeaderSize);
+    }
+}
