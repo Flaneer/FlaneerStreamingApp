ff3da5b6fc1d1b9b4b1603f8fdd5555f79235db8
thoxey <tom@flaneer.com>
2022-05-24 11:55:20 +0100

    Changing prefix

--- FlaneerMediaLib/Logging/LoggerFactory.cs
+++ FlaneerMediaLib/Logging/LoggerFactory.cs
@@ -1,4 +1,5 @@
-﻿using Spectre.Console;
+﻿using System.Diagnostics;
+using Spectre.Console;
 
 namespace FlaneerMediaLib.Logging;
 
@@ -11,36 +12,42 @@ internal class LoggerFactory
 
     // ReSharper disable once ConstantNullCoalescingCondition
     private static LoggerFactory Instance => instance ??= new LoggerFactory();
+    
+    private string TimeString() => $"[gray]<{DateTime.Now.ToString("HH:mm:ss")}>[/]";
 
-    private List<string> log = new();
+    private string GetLogPrefix(string formatting, string typeString)
+    {
+        StackTrace st = new StackTrace(true);
+        StackFrame sf = st.GetFrame(3);
+        var method = $"{sf.GetMethod().Name}";
+        var fileLineNumber = $"{sf.GetFileLineNumber()}";
 
+        return $"[{formatting}]<{typeString}:{fileLineNumber}[/][gray]({method})[/][{formatting}]>[/]";
+    }
+    
     internal static Logger CreateLogger(object obj) => new Logger(obj.GetType(), Instance);
 
     internal void Info(string s, string typeString)
     {
-        var message = $"[bold green]<INFO :{typeString}>[/] {s}";
+        var message = $"{TimeString()}{GetLogPrefix("bold green", typeString)} {s}";
         AnsiConsole.MarkupLine(message);
-        log.Add(message);
     }
     
     public void Debug(string s, string typeString)
     {
-        var message = $"[bold yellow]<DEBUG:{typeString}>[/] {s}";
+        var message = $"{TimeString()}{GetLogPrefix("bold yellow", typeString)} {s}";
         AnsiConsole.MarkupLine(message);
-        log.Add(message);
     }
     
     internal void Error(Exception exception, string typeString)
     {
-        var message = $"[bold red]<ERROR:{typeString}>[/] {exception}";
+        var message = $"{TimeString()}{GetLogPrefix("bold red", typeString)} {exception}";
         AnsiConsole.MarkupLine(message);
-        log.Add(message);
     }
 
     internal void Error(string exception, string typeString)
     {
-        var message = $"[bold red]<ERROR:{typeString}>[/] {exception}";
+        var message = $"{TimeString()}{GetLogPrefix("bold red", typeString)} {exception}";
         AnsiConsole.MarkupLine(message);
-        log.Add(message);
     }
 }
