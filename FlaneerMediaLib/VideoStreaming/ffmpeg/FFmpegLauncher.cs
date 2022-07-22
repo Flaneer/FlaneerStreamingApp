3d2ea75e83d3852b06e4d65e08e698a4e2786816
thoxey <tom@flaneer.com>
2022-07-22 14:21:52 +0100

    Cleaning ffmpeg launcher

--- FlaneerMediaLib/VideoStreaming/ffmpeg/FFmpegLauncher.cs
+++ FlaneerMediaLib/VideoStreaming/ffmpeg/FFmpegLauncher.cs
@@ -1,7 +1,5 @@
 ﻿using System.Runtime.InteropServices;
 using FFmpeg.AutoGen;
-using FlaneerMediaLib.Logging;
-
 using FF = FFmpeg.AutoGen.ffmpeg;
 
 namespace FlaneerMediaLib.VideoStreaming.ffmpeg
@@ -9,19 +7,8 @@ namespace FlaneerMediaLib.VideoStreaming.ffmpeg
     /// <summary>
     /// Loads the ffmpeg binaries
     /// </summary>
-    public class FFmpegLauncher
+    public static class FFmpegLauncher
     {
-        private Logger logger;
-        
-        private static FFmpegLauncher instance = null!;
-
-        private FFmpegLauncher()
-        {
-        }
-
-        // ReSharper disable once ConstantNullCoalescingCondition
-        private static FFmpegLauncher Instance => instance ??= new FFmpegLauncher();
-        
         /// <summary>
         /// Start ffmpeg
         /// </summary>
@@ -53,47 +40,7 @@ namespace FlaneerMediaLib.VideoStreaming.ffmpeg
             FF.av_log_set_callback(logCallback);
         }
         
-        /// <summary>
-        /// TODO: Make this work safely, it is a nice to have so not spending any more time now
-        /// </summary>
-        /// <param name="avcl">A pointer to an arbitrary struct of which the first field is a pointer to an AVClass struct.</param>
-        /// <param name="level">level	The importance level of the message expressed using a Logging Constant. </param>
-        /// <param name="fmt">The format string (printf-compatible) that specifies how subsequent arguments are converted to output. </param>
-        /// <param name="vl">The arguments referenced by the format string. </param>
-        private static unsafe void FlaneerLogCallback(void* avcl, int level, [MarshalAs((UnmanagedType) 0)] string fmt, byte* vl)
-        {
-            var lineSize = 1024;
-            var lineBuffer = stackalloc byte[lineSize];
-            var printPrefix = 1;
-            FF.av_log_format_line(avcl, level, fmt, vl, lineBuffer, lineSize, &printPrefix);
-            var line = Marshal.PtrToStringAnsi((IntPtr) lineBuffer);
-            line ??= "";
         
-            switch (level)
-            {
-                case FF.AV_LOG_DEBUG:
-                    Instance.logger.Debug(line);
-                    break;
-                case FF.AV_LOG_ERROR:
-                case FF.AV_LOG_FATAL:
-                case FF.AV_LOG_PANIC:
-                case FF.AV_LOG_WARNING:
-                    Instance.logger.Error(line);
-                    break;
-                case FF.AV_LOG_INFO:
-                    Instance.logger.Info(line);
-                    break;
-                case FF.AV_LOG_QUIET:
-                case FF.AV_LOG_TRACE:
-                case FF.AV_LOG_VERBOSE:
-                    Instance.logger.Trace(line);
-                    break;
-                default:
-                    Instance.logger.Trace(line);
-                    break;
-            }
-            
-        }
 
         private static void RegisterBinaries()
         {
@@ -120,5 +67,47 @@ namespace FlaneerMediaLib.VideoStreaming.ffmpeg
             else
                 throw new NotSupportedException();
         }
+        
+        /*/// <summary>
+         /// TODO: Make this work safely, it is a nice to have so not spending any more time now
+         /// </summary>
+         /// <param name="avcl">A pointer to an arbitrary struct of which the first field is a pointer to an AVClass struct.</param>
+         /// <param name="level">level	The importance level of the message expressed using a Logging Constant. </param>
+         /// <param name="fmt">The format string (printf-compatible) that specifies how subsequent arguments are converted to output. </param>
+         /// <param name="vl">The arguments referenced by the format string. </param>
+         private static unsafe void FlaneerLogCallback(void* avcl, int level, [MarshalAs((UnmanagedType) 0)] string fmt, byte* vl)
+         {
+             var lineSize = 1024;
+             var lineBuffer = stackalloc byte[lineSize];
+             var printPrefix = 1;
+             FF.av_log_format_line(avcl, level, fmt, vl, lineBuffer, lineSize, &printPrefix);
+             var line = Marshal.PtrToStringAnsi((IntPtr) lineBuffer);
+             line ??= "";
+         
+             switch (level)
+             {
+                 case FF.AV_LOG_DEBUG:
+                     Instance.logger.Debug(line);
+                     break;
+                 case FF.AV_LOG_ERROR:
+                 case FF.AV_LOG_FATAL:
+                 case FF.AV_LOG_PANIC:
+                 case FF.AV_LOG_WARNING:
+                     Instance.logger.Error(line);
+                     break;
+                 case FF.AV_LOG_INFO:
+                     Instance.logger.Info(line);
+                     break;
+                 case FF.AV_LOG_QUIET:
+                 case FF.AV_LOG_TRACE:
+                 case FF.AV_LOG_VERBOSE:
+                     Instance.logger.Trace(line);
+                     break;
+                 default:
+                     Instance.logger.Trace(line);
+                     break;
+             }
+             
+         }*/
     }
 }
