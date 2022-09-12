3e3b86ef7e2957abe866ded8614e60c7e3434543
thoxey <tom@flaneer.com>
2022-09-12 11:05:57 +0100

    Removing Logging

--- FFmpegDecoder/AVIOReader.cpp
+++ FFmpegDecoder/AVIOReader.cpp
@@ -8,23 +8,9 @@ static int read(void* opaque, unsigned char* buf, int buf_size)
     auto bd = static_cast<struct buffer_data*>(opaque);
     buf_size = FFMIN(buf_size, bd->size);
 
-    std::cout << "AVIO In:";
-    for (int i = 0; i < 30; ++i)
-    {
-        std::cout << *(bd->ptr + i);
-    }
-    std::cout << "\n";
-
     memcpy(buf, bd->ptr, buf_size);
     bd->ptr += buf_size;
     bd->size -= buf_size;
-
-    std::cout << "AVIO Buf:";
-    for (int i = 0; i < 30; ++i)
-    {
-        std::cout << *(buf + i);
-    }
-    std::cout << "\n";
 
     return buf_size;
 }
@@ -40,12 +26,6 @@ void AVIOReader::SetBuffer(buffer_data buffer)
     bufferSize = buffer.size;
     bufferPtr = static_cast<unsigned char*>(av_malloc(bufferSize));
     memcpy(bufferPtr, buffer.ptr, bufferSize);
-    std::cout << "New Buffer: ";
-    for (int i = 0; i < 30; ++i)
-    {
-        std::cout << *(bufferPtr + i);
-    }
-    std::cout << "\n";
     AllocAvioContext();
 }
 
