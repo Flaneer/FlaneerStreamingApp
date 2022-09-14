c0e1cfe38c89060b1bacfa7f31e9a620561ac2fe
thoxey <tom@flaneer.com>
2022-09-14 17:45:28 +0200

    Removed hard coded GoPs and updated write to file logic

--- FlaneerMediaLib/VideoStreaming/UDPVideoSink.cs
+++ FlaneerMediaLib/VideoStreaming/UDPVideoSink.cs
@@ -17,20 +17,29 @@ public class UDPVideoSink : IVideoSink
     private readonly UDPSender udpSender;
     
     private readonly Logger logger;
-
+    private readonly VideoSettings videoSettings;
     private int encodeCount;
     private TimeSpan totalEncodeTime;
+
+    private FileStream stream;
+
+    private int sentPacketCount;
+    private bool alsoWriteToFile = false;
 
     /// <summary>
     /// ctor
     /// </summary>
-    public UDPVideoSink()
+    public UDPVideoSink(VideoSettings videoSettings)
     {
         logger = Logger.GetLogger(this);
         ServiceRegistry.TryGetService(out udpSender);
         
+        if(alsoWriteToFile)
+            stream = new FileStream("out.h264", FileMode.Create, FileAccess.Write);
+
         GetEncoder();
         GetSource();
+        this.videoSettings = videoSettings;
     }
 
     private void GetEncoder()
@@ -110,9 +119,6 @@ public class UDPVideoSink : IVideoSink
         stopWatch.Stop();
     }
 
-    private int sentPacketCount;
-    private bool alsoWriteToFile  = false;
-
     private unsafe void SendFrame(UnmanagedVideoFrame frame, TimeSpan frameTime)
     {
         using var uStream = new UnmanagedMemoryStream((byte*) frame.FrameData, frame.FrameSize);
@@ -122,7 +128,7 @@ public class UDPVideoSink : IVideoSink
         WriteToFile(frameBytes);
         
         var numberOfPackets = (byte) Math.Ceiling((double)frame.FrameSize / VideoUtils.FRAMEWRITABLESIZE);
-        
+
         var sent = 0;
         for (byte i = 0; i < numberOfPackets; i++)
         {
@@ -134,11 +140,8 @@ public class UDPVideoSink : IVideoSink
                 PacketIdx = i,
                 FrameDataSize = frame.FrameSize,
                 SequenceIDX = nextFrame,
+                IsIFrame = nextFrame % videoSettings.GoPLength == 0
             };
-
-            //TODO: set this properly
-            var gopLength = 5;
-            frameHeader.IsIFrame = frameHeader.SequenceIDX % gopLength == 0; 
             
             var packetSize = Math.Min(VideoUtils.FRAMEWRITABLESIZE, frame.FrameSize - sent);
             var transmissionArraySize = TransmissionVideoFrame.HeaderSize + packetSize;
@@ -167,9 +170,7 @@ public class UDPVideoSink : IVideoSink
         {
             try
             {
-                var f = File.Open("in.h264", FileMode.Append);
-                f.Write(frameBytes);
-                f.Close();
+                stream.Write(frameBytes);
             }
             catch (Exception e)
             {
