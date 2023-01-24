de4d47f7921c0d5ebb42442e2e4dd54128b66058
thoxey <tom@flaneer.com>
2023-01-24 15:42:06 +0000

    Added config file CLI option

--- MediaLibTests/CommandLineArgumentStoreTests.cs
+++ MediaLibTests/CommandLineArgumentStoreTests.cs
@@ -1,4 +1,5 @@
-﻿using FlaneerMediaLib;
+﻿using System.IO;
+using FlaneerMediaLib;
 using Xunit;
 
 namespace MediaLibTests;
@@ -25,5 +26,35 @@ public class CommandLineArgumentStoreTests
         
         var arg3Params = clArgStore.GetParams("arg3");
         Assert.Equal(new string[]{"param1", "param2", "param3"}, arg3Params);
+    }
+
+    [Fact]
+    public void TestParseFromFile()
+    {
+        var rawString = "[Streaming]\narg1 = 1234\narg2 = 5678 9012\n-arg3 = 3456";
+        File.WriteAllText("FlaneerStreamingConfig.ini", rawString);
+        
+        ServiceRegistry.ClearRegistry();
+        CommandLineArgumentStore.CreateAndRegister(new []{"-arg3", "777"});
+        ServiceRegistry.TryGetService<CommandLineArgumentStore>(out var clArgStore);
+        
+        
+        //Test single param
+        Assert.True(clArgStore.HasArgument("arg1"));
+        
+        var arg1Params = clArgStore.GetParams("arg1");
+        Assert.Equal(new[]{"1234"}, arg1Params);
+        
+        //Test double param
+        Assert.True(clArgStore.HasArgument("arg2"));
+        
+        var arg2Params = clArgStore.GetParams("arg2");
+        Assert.Equal(new[]{"5678", "9012"}, arg2Params);
+        
+        //Test override
+        Assert.True(clArgStore.HasArgument("arg3"));
+        
+        var arg3Params = clArgStore.GetParams("arg3");
+        Assert.Equal(new[]{"777"}, arg3Params);
     }
 }
